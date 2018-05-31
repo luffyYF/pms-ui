b600f647a4c47593a08e26195d812a06f0953b65
wjf <512130254@qq.com>
2018-05-31 09:49:33 +0800

    配置文件

--- config/index.js
+++ config/index.js
@@ -11,9 +11,9 @@ module.exports = {
     assetsPublicPath: '/',  
     proxyTable: {
       '/pms': {
-        // target: "http://10.40.36.227:7777/pms",//设置你调用的接口域名和端口号 别忘了加http
+        target: "http://10.40.36.227:7777/pms",//设置你调用的接口域名和端口号 别忘了加http
         // target: "http://10.40.36.76:8888/pms",//设置你调用的接口域名和端口号 别忘了加http
-        target: "http://www.housefeel.cn/test-pms",
+        // target: "http://www.housefeel.cn/test-pms",
         secure: false,      // 如果是https接口，需要配置这个参数
         changeOrigin: true,  // 跨域带cookies
         pathRewrite: {
