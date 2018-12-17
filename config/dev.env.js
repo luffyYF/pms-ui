ede439bdc02760cc19521c6dda12a8b612baf587
longz <2424511209@qq.com>
2018-12-17 10:25:58 +0800

    修改配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,7 +7,7 @@ module.exports = merge(prodEnv, {
   API_ROOT: '"/pms"',
   IMAGE_ROOT: '"/fs"',
   UPMS_ROOT:'"http://192.168.5.8:8080/upms-back/"',
-  OA_ROOT:'"http://192.168.5.10:8085"',
+  OA_ROOT:'"http://192.168.5.11:8085"',
   PRINT_ROOT: '"http://192.168.5.11:8085"',
   SERVER_FLAG:false,//是否是正式服标识 true是正式服 false本地测试
 })
