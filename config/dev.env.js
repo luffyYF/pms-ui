c620a804049f84f1b7c9b758bfb984bbf99a7b87
chao <chao_c_c@163.com>
2019-02-27 18:09:08 +0800

    客房管理-删除房间后，再添加已删除的房间号不能再添加

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_ROOT: '"/pms"',
   IMAGE_ROOT: '"/fs"',
-  UPMS_ROOT:'"http://192.168.5.3:8084"',
+  UPMS_ROOT:'"http://192.168.5.10:8080"',
   UPLOAD_ROOT: '"http://localhost"',//上传文件的地址 微云系统
   OA_ROOT:'"http://192.168.5.3:8085"',
   PRINT_ROOT: '"http://192.168.5.3:8084/"',
