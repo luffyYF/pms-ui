d11e79338d01648cbe4dc1faa634488d5313cfe0
chao <chao_c_c@163.com>
2019-03-18 10:43:06 +0800

    *批量添加客人

---
+++ src/api/order/pmsIdCardInfoController.js
@@ -0,0 +1,10 @@
+import request from '@/utils/requestJson'
+
+//身份证列表
+export const list = params => {
+  return request.get('/back/orderIdCardInfo/list', {params:params})
+}
+//保存
+export const add = params => {
+  return request.post('/back/orderIdCardInfo/add', params)
+}
