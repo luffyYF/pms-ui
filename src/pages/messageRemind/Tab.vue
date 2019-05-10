2882bc2e2bbdabd0825abd15da0a562aa4d5ff0e
wjf <512130254@qq.com>
2019-05-10 16:28:10 +0800

    配置正式服websocket路径

--- src/pages/messageRemind/Tab.vue
+++ src/pages/messageRemind/Tab.vue
@@ -52,45 +52,46 @@ export default {
         console.log("您的浏览器不支持WebSocket");
       }else{
         console.log("您的浏览器支持WebSocket");
-        //实现化WebSocket对象，指定要连接的服务器地址与端口  建立连接  
+        //实现化WebSocket对象，指定要连接的服务器地址与端口  建立连接
         //等同于socket = new WebSocket("ws://localhost:8083/checkcentersys/websocket/20");
         let uuid = this.createUUID()
-        let url = "http://localhost:8084/websocket/"+uuid;
-        socket = new WebSocket(url.replace("http","ws"));  
+        // let url = "http://www.housefeel.cn/pms-back/websocket/"+uuid;
+        let url = process.env.API_ROOT + "/websocket/"+uuid;
+        socket = new WebSocket(url.replace("http","ws"));
         //打开事件
         socket.onopen = function() {
           console.log("Socket 已打开");
-          //socket.send("这是来自客户端的消息" + location.href + new Date());  
+          //socket.send("这是来自客户端的消息" + location.href + new Date());
         };
-        //获得消息事件  
-        socket.onmessage = this.remindController;  
-        //关闭事件  
-        socket.onclose = function() {  
-          console.log("Socket已关闭");  
-        };  
-        //发生了错误事件  
-        socket.onerror = function() {  
-          alert("Socket发生了错误");  
+        //获得消息事件
+        socket.onmessage = this.remindController;
+        //关闭事件
+        socket.onclose = function() {
+          console.log("Socket已关闭");
+        };
+        //发生了错误事件
+        socket.onerror = function() {
+          alert("Socket发生了错误");
           //此时可以尝试刷新页面
-        }  
+        }
         //离开页面时，关闭socket
         //jquery1.8中已经被废弃，3.0中已经移除
-        // $(window).unload(function(){  
-        //     socket.close();  
-        //});  
+        // $(window).unload(function(){
+        //     socket.close();
+        //});
       }
     },
-    createUUID() {  
-      var s = [];  
-      var hexDigits = "0123456789abcdef";  
-      for (var i = 0; i < 36; i++) {  
-        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);  
-      }  
-      s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010  
-      s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01  
-      s[8] = s[13] = s[18] = s[23] = "-";  
-      var uuid = s.join("");  
-      return uuid;  
+    createUUID() {
+      var s = [];
+      var hexDigits = "0123456789abcdef";
+      for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+      }
+      s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+      s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+      s[8] = s[13] = s[18] = s[23] = "-";
+      var uuid = s.join("");
+      return uuid;
     },
 
   }
