c5889387abe1b5197a81de20440b61a774285acd
yinfan <253415528@qq.com>
2019-05-29 18:30:52 +0800

    交班报表

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     // host: '10.40.36.9', // can be overwritten by process.env.HOST
     port: 8082, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
--- src/api/shiftData/shiftData.js
+++ src/api/shiftData/shiftData.js
@@ -2,21 +2,31 @@ import request from '@/utils/requestJson'
 
 // 预览
 export const getPreShiftInf = params => {
-    return request.get('/back/shiftData/getPreShiftInf', {params:params})
-} 
+    return request.get('/back/shiftData/getPreShiftInf', { params: params })
+}
 
 // 添加
 export const addShiftData = params => {
     return request.post('/back/shiftData/addShiftData', params)
-} 
+}
 
 // 查找
 export const listShiftData = params => {
-    return request.get('/back/shiftData/listShiftData', {params:params})
-} 
+    return request.get('/back/shiftData/listShiftData', { params: params })
+}
 
 // 查找
 export const shiftDataView = params => {
-    return request.get('/back/shiftData/shiftDataView', {params:params})
-} 
+    return request.get('/back/shiftData/shiftDataView', { params: params })
+}
+
+// ip:192.168.5.3:8084
+// url:/back/shiftData/shiftDataViewByPk
+// @param shiftDataPk 交班数据主键
+// @return
+//查看交班报表
+export const shiftDataViewByPk = params => {
+    return request.get('/back/shiftData/shiftDataViewByPk', { params: params })
+}
+
 
