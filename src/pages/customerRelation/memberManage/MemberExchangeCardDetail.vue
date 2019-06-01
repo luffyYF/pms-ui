7308dee736b777d84928445ce013929c25ff5313
wind <2687557963@qq.com>
2019-06-01 19:10:32 +0800

    楼层修复

---
+++ src/pages/customerRelation/memberManage/MemberExchangeCardDetail.vue
@@ -0,0 +1,86 @@
+// 会员积分明细编辑
+// Created by Administrator on 2019-02-21T16:46:19.175.
+<template>
+  <div>
+      <el-table
+        :data="rows"
+        border
+        v-loading="loading"
+        style="width: 100%"
+        max-height="410px">
+        <el-table-column prop="roomNumber" label="房号"></el-table-column>
+        <el-table-column prop="moneyAmount" label="总消费"></el-table-column>
+        <el-table-column prop="integral" label="积分"></el-table-column>
+        <el-table-column prop="type" label="积分类型">
+          <template slot-scope="scope">
+            <span v-if="scope.row.type == 0">现金充值</span>
+            <span v-else-if="scope.row.type == 1">银行卡充值</span>
+            <span v-else-if="scope.row.type == 2">支付宝充值</span>
+            <span v-else-if="scope.row.type == 3">微信充值</span>
+            <span v-else-if="scope.row.type == 4">充值赠送金额</span>
+            <span v-else-if="scope.row.type == 5">充值赠送</span>
+            <span v-else-if="scope.row.type == 6">积分调整</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="createUserName" label="操作员"></el-table-column>
+        <el-table-column prop="createTime" label="积分时间" width="184"></el-table-column>
+        <el-table-column prop="remark" label="备注"></el-table-column>
+      </el-table>
+      <el-pagination
+        style="margin: 10px 20px;"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="1"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+  </div>
+</template>
+
+<script>
+import { listMemberLog } from '@/api/customerRelation/pmsMemberLogController'
+
+  export default {
+    data () {
+      return {
+        loading: false,
+        queryParams: {
+          pageNum: 1,
+          pageSize: 10,
+          memPk: null,
+          type: null,
+        },
+        rows: [],
+        total: 0,
+      }
+    },
+    methods: {
+      init(id, type) {
+        this.queryParams.memPk = id
+        this.queryParams.type = type
+        this.listSearch()
+      },
+      listSearch () {
+        this.loading = false
+        listMemberLog(this.queryParams).then(res => {
+          this.rows = res.data.list
+          this.total = parseInt(res.data.total)
+        }).finally(() => {
+          this.loading = false
+        })
+      },
+      // 分页相关
+      handleSizeChange (val) {
+        this.queryParams.pageSize = val
+        this.listSearch()
+      },
+      // 分页相关
+      handleCurrentChange (val) {
+        this.queryParams.pageNum = val
+        this.listSearch()
+      },
+    }
+  }
+</script>
--- src/pages/customerRelation/memberManage/MemberInfoDialog.vue
+++ src/pages/customerRelation/memberManage/MemberInfoDialog.vue
@@ -4,11 +4,11 @@
         <!-- 会员管理 dialog -->
         <el-dialog title="会员管理" :visible.sync="dialogMemberVisible" width="930px" :before-close="handleClose" class="dialogMemberManage">
             <el-row>
-                <el-button size="mini" type="primary">换卡</el-button>
+                <el-button size="mini" type="primary" @click="memberExchangeCard(membeInfo)" :disabled="membeInfo.rechargeFlag == 'N'">换卡</el-button>
                 <el-button size="mini" type="primary">补卡</el-button>
-                <el-button size="mini" type="primary">改密码</el-button>
+                <el-button size="mini" type="primary" @click="memberUpdatePasswordClick(membeInfo)" :disabled="membeInfo.rechargeFlag == 'N'">改密码</el-button>
                 <el-button size="mini" type="primary"  @click="memberRechargeClick(membeInfo)"  :disabled="membeInfo.rechargeFlag == 'N'">充值</el-button>
-                <el-button size="mini" type="primary">积分增减</el-button>
+                <el-button size="mini" type="primary" @click="memberIntegralExchangeClick(membeInfo)" :disabled="membeInfo.rechargeFlag == 'N'">积分增减</el-button>
                 <el-button size="mini" type="primary">积分兑换</el-button>
                 <el-button size="mini" type="primary">积分换房</el-button>
                 <el-button size="mini" type="primary">会员升级</el-button>
@@ -32,8 +32,8 @@
                 <el-tab-pane label="积分明细" name="MemberIntegralDetailTable">
                     <MemberIntegralDetailTable ref="MemberIntegralDetailTable" />
                 </el-tab-pane>
-                <el-tab-pane label="换卡明细" name="1">
-
+                <el-tab-pane label="换卡明细" name="MemberExchangeCardDetail">
+                    <MemberExchangeCardDetail ref="MemberExchangeCardDetail"/>
                 </el-tab-pane>
                 <el-tab-pane label="积分兑换明细" name="2">
 
@@ -49,6 +49,10 @@
                 
         </el-dialog>
         <member-recharge ref="memberRechargeRefs" @callback="memberListData(form.pageNum)"></member-recharge>
+        <member-integral-exchange ref="memberIntegralExchangeRefs" @callback="memberListData(form.pageNum)"></member-integral-exchange>
+        <member-update-password ref="memberUpdatePasswordRefs" @callback="memberListData(form.pageNum)"></member-update-password>
+        <member-exchange-card ref="memberExchangeCardRefs" @callback="memberListData(form.pageNum)"></member-exchange-card>
+
     </div>
 </template>
 
@@ -56,16 +60,21 @@
 
 import MemberInfo from "./MemberInfo";
 import MemberRecharge from "./MemberRecharge/MemberRecharge.vue"
+import MemberIntegralExchange from "./MemberIntegral/MemberIntegralExchange.vue"
 import MemberRechargeDetailDialog from "./MemberRecharge/MemberRechargeDetailDialog.vue"
 import MemberIntegralDetailDialog from "./MemberIntegral/MemberIntegralDetailDialog.vue"
+import MemberExchangeCardDetailDialog from "./MemberExchangeCardDetailDialog.vue"
+import MemberUpdatePassword from "./MemberUpdatePassword.vue";
 import MemberConsumptionDetailDialog from "./MemberConsumption/MemberConsumptionDetailDialog.vue"
+import MemberExchangeCard from "./MemberExchangeCard.vue"
+import MemberExchangeCardDetail from "./MemberExchangeCardDetail.vue"
 
 import MemberRechargeTable from "./MemberRecharge/MemberRechargeDetailTable.vue"
 import  MemberConsumptionDetailTable from './MemberConsumption/MemberConsumptionDetailTable.vue'
 import  MemberIntegralDetailTable from './MemberIntegral/MemberIntegralDetailTable.vue'
 
 export default {
-    components: { MemberInfo, MemberRecharge, MemberRechargeDetailDialog, MemberIntegralDetailDialog, MemberConsumptionDetailDialog,MemberRechargeTable,MemberConsumptionDetailTable,MemberIntegralDetailTable },
+    components: { MemberInfo, MemberRecharge,MemberIntegralExchange,MemberUpdatePassword,MemberExchangeCard, MemberRechargeDetailDialog, MemberIntegralDetailDialog, MemberConsumptionDetailDialog,MemberRechargeTable,MemberConsumptionDetailTable,MemberIntegralDetailTable,MemberExchangeCardDetail,MemberExchangeCardDetailDialog },
   data() {
     return {
       dialogMemberVisible: false,
@@ -96,6 +105,10 @@ export default {
             this.$nextTick(()=>{
                 this.$refs.MemberIntegralDetailTable.init(this.membeInfo.memPk,1)
             })
+        }else if(this.activeName == "MemberExchangeCardDetail"){
+            this.$nextTick(()=>{
+                this.$refs.MemberExchangeCardDetail.init(this.membeInfo.memPk,1)
+            })
         }
         else if(this.activeName){
             this.$refs[this.activeName].init()
@@ -119,6 +132,15 @@ export default {
     },
     memberRechargeClick (row) {
         this.$refs.memberRechargeRefs.showDialog(row,false)
+    },
+     memberIntegralExchangeClick (row) {
+        this.$refs.memberIntegralExchangeRefs.showDialog(row,false)
+    },
+    memberUpdatePasswordClick(row){
+        this.$refs.memberUpdatePasswordRefs.showDialog(row,false)
+    },
+    memberExchangeCard(row){
+        this.$refs.memberExchangeCardRefs.showDialog(row,false);
     },
 
   }
